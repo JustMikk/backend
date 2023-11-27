dd4fdf33a16b6165972a5a280f8f9b81acf3b483
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2023-11-26 21:49:17 -0800

    All done for deployment

--- api/settings.py
+++ api/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://csecastu.pythonanywhere.com"]
 
 
 # Application definition
@@ -47,11 +47,7 @@ MIDDLEWARE = [
 AUTH_USER_MODEL = "accounts.User"
 
 CORS_ALLOWED_ORIGINS = [
-    "https://csec-sigma.vercel.app",
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:5173",
+    "https://csecastu.pythonanywhere.com",
 ]
 
 CORS_ALLOW_METHODS = (
