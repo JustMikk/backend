23ce33fd43661c070e1cc21069864a6f94d5eedd
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2023-11-26 22:13:51 -0800

    Done

--- api/settings.py
+++ api/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://csecastu.pythonanywhere.com"]
+ALLOWED_HOSTS = ["https://csecastu.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
